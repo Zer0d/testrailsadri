6590dd4b559118817b94e9831d568bfdf0d80f9b
zerod <zerod@dius.com.au>
2012-10-08 06:52:02 +1100

    Initial zerod configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZerodRailsTemplate::Application.config.secret_token = '45af8c4cfac60c6a31f8b85d245af4e9399465cb0f41602e10c24c7ce662ba72b726237c51c42f5044f7cd1d6ecdd64f2c69f0015ac23e50c3ca313966da7153'
